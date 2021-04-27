13c8857796a054c3e957fa58e32854292fda3efc
Glitch (ko-playlist-project) <none>
2021-04-27 22:12:55 +0000

    ⛲️💵 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,11 +1,47 @@
-let songList = ["Depature", "Tell Me", "The Day", "Classic Man"]
-let songAuthors = ["MOL$, Yun Head", "RAFA, lullaboy", "NateWantsToBattle", "Jidenna, Roman GianArthur"]
-let songLength = ["2:09", "3:27", "1:35", "3:46"]
-let songImages = ["https://images.genius.com/8f9c67f4b811c7c1c7cf46f9e52ec0d3.1000x1000x1.png","https://i1.sndcdn.com/artworks-M6fqaOorgqByUn8Q-mM4ymg-t500x500.jpg","https://i.scdn.co/image/ab67616d0000b273e70890c397046a2ebac00203","https://upload.wikimedia.org/wikipedia/en/d/d5/Classic_Man_Art.jpg"]
-let songLinks = ["https://www.youtube.com/watch?v=LLuxpRT6G5o", "https://www.youtube.com/watch?v=F_j7CdtAN00", "https://www.youtube.com/watch?v=p4H5ul0cW2s", "https://www.youtube.com/watch?v=R9SZI4zdW-o"]
+let songList = ["Depature", "Tell Me", "The Day", "Classic Man"];
+let songAuthors = [
+  "MOL$, Yun Head",
+  "RAFA, lullaboy",
+  "NateWantsToBattle",
+  "Jidenna, Roman GianArthur"
+];
+let songLength = ["2:09", "3:27", "1:35", "3:46"];
+let songImages = [
+  "https://images.genius.com/8f9c67f4b811c7c1c7cf46f9e52ec0d3.1000x1000x1.png",
+  "https://i1.sndcdn.com/artworks-M6fqaOorgqByUn8Q-mM4ymg-t500x500.jpg",
+  "https://i.scdn.co/image/ab67616d0000b273e70890c397046a2ebac00203",
+  "https://upload.wikimedia.org/wikipedia/en/d/d5/Classic_Man_Art.jpg"
+];
+let songLinks = [
+  "https://www.youtube.com/watch?v=LLuxpRT6G5o",
+  "https://www.youtube.com/watch?v=F_j7CdtAN00",
+  "https://www.youtube.com/watch?v=p4H5ul0cW2s",
+  "https://www.youtube.com/watch?v=R9SZI4zdW-o"
+];
 
 function displaySongInfo() {
-  // Complete the Day 2 goals inside this function
+  let counter = 0;
+  for (let songnames of songList) {
+    $(".container").append(
+      "<div>   <span class='column images'><img src='" +
+        songImages[counter] +
+        "' ></span>" +
+        "   <span class='column songs'>" +
+        songnames +
+        "</span>" +
+        "  <span class='column artists'>" +
+        songAuthors[counter] +
+        "</span>" +
+        "   <span class='column lengths'>" +
+        songLength[counter] +
+        "</span>" +
+        "    <span class='column links'><a href='" +
+        songLinks[counter] +
+        "'>Link</a>" +
+        "</span></div>"
+    );
+    counter++;
+  }
 }
 
 function emptySongInfo() {
@@ -24,7 +60,7 @@ function addSongInfo() {
   // Complete Day 3 goals inside this function
 }
 
-$("#add").click(function () {
+$("#add").click(function() {
   emptySongInfo();
   addSongInfo();
   displaySongInfo();
